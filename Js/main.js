2b5c99a3646ebd0ea4dd3342e461607810bdb47a
Mesach Venegas <mesach.venegas@hotmail.com>
2022-11-30 20:19:50 -0800

    add: menu functions and scroll efects

--- Js/main.js
+++ Js/main.js
@@ -4,8 +4,22 @@ const toggleMenu = () =>{
     const menu =  document.getElementById('menu-options');
     const upStripe = document.getElementById('up-stripe');
     const downStripe = document.getElementById('down-stripe');
+    const links = document.querySelectorAll('.nav__menu__link')
     btnMenu.addEventListener('click', e => {
         menu.classList.toggle('nav__menu--open')
+        upStripe.classList.toggle('menu__stripe__up--open')
+        downStripe.classList.toggle('menu__stripe__down--open')
+    })
+
+    menu.addEventListener('click', e => {
+        links.forEach(link => {
+            link.classList.remove('active')
+        })
+        console.log(e.target)
+        if(e.target.tagName == "A"){
+            e.target.classList.add('active')
+        }
+        menu.classList.toggle('nav__menu--open');
         upStripe.classList.toggle('menu__stripe__up--open')
         downStripe.classList.toggle('menu__stripe__down--open')
     })
@@ -20,8 +34,59 @@ const mode = () =>{
     })
 }
 
+const scrollUp = () =>{
+    const scrollBtn = document.getElementById('up-btn');
+    const about = document.getElementById('about-menu'); //690
+    const portfolio = document.getElementById('portfolio-menu') //2113
+    const contact = document.getElementById('contact-menu') //3792
+    const aboutBar = document.getElementById('about-bar'); //690
+    const portfolioBar = document.getElementById('portfolio-bar') //2113
+    const contactBar = document.getElementById('contact-bar') //3792
+
+    scrollBtn.addEventListener('click', e =>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    })
+    window.onscroll = () =>{
+        if(window.scrollY < 650){
+            about.classList.remove('active')
+            aboutBar.classList.remove('bar__active')
+        }
+        if(window.scrollY > 650){
+            about.classList.add('active')
+            aboutBar.classList.add('bar__active')
+            portfolio.classList.remove('active')
+            portfolioBar.classList.remove('bar__active')
+        }
+        if (window.scrollY > 2100){
+            about.classList.remove('active')
+            aboutBar.classList.remove('bar__active')
+            portfolio.classList.add('active')
+            portfolioBar.classList.add('bar__active')
+            contact.classList.remove('active')
+            contactBar.classList.remove('bar__active')
+        }
+        if(window.scrollY > 3000){
+            portfolio.classList.remove('active')
+            portfolioBar.classList.remove('bar__active')
+            contact.classList.add('active')
+            contactBar.classList.add('bar__active')
+        }
+
+        if(window.scrollY < 490){
+            scrollBtn.classList.remove('up__btn--active')
+        }else {
+            scrollBtn.classList.add('up__btn--active')
+        }
+    }
+
+}
+
 
 document.addEventListener('DOMContentLoaded',() => {
     toggleMenu()
     mode()
+    scrollUp()
 })
