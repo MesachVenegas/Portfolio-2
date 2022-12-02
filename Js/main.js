33c7211451482bc72ddb2ae1218a97e09397df7b
Mesach Venegas <mesach.venegas@hotmail.com>
2022-12-01 17:19:02 -0800

    change: activation menu zone

--- Js/main.js
+++ Js/main.js
@@ -69,7 +69,7 @@ const scrollUp = () =>{
             contact.classList.remove('active')
             contactBar.classList.remove('bar__active')
         }
-        if(window.scrollY > 2650){
+        if(window.scrollY > 2550){
             portfolio.classList.remove('active')
             portfolioBar.classList.remove('bar__active')
             contact.classList.add('active')
